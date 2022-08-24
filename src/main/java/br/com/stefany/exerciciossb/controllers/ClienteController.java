763c47e6323cb834965238137d5e5e68d2b6eed0
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-24 19:00:56 -0300

    add @ Passando Parâmetros para Web Service #01

--- src/main/java/br/com/stefany/exerciciossb/controllers/ClienteController.java
+++ src/main/java/br/com/stefany/exerciciossb/controllers/ClienteController.java
@@ -1,8 +1,6 @@
 package br.com.stefany.exerciciossb.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.stefany.exerciciossb.models.Cliente;
 
@@ -13,5 +11,10 @@ public class ClienteController {
     @GetMapping("/qualquer")
     public Cliente obterCliente() {
         return new Cliente(28, "Pedro", "123.456.789-00");
+    }
+
+    @GetMapping("/{id}")
+    public Cliente obterClientePorId1(@PathVariable int id) {
+        return new Cliente(id, "Maria", "987.654.311-00");
     }
 }
