e3d27a466d77a0cce23e62e79e155902be891e48
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-24 19:23:10 -0300

    add @ Desafio Web Service Calculadora

---
+++ src/main/java/br/com/stefany/exerciciossb/controllers/CalculadoraController.java
@@ -0,0 +1,22 @@
+package br.com.stefany.exerciciossb.controllers;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/calculadora")
+public class CalculadoraController {
+
+    @GetMapping("/somar/{a}/{b}")
+    public int somar(@PathVariable int a, @PathVariable int b) {
+        return a + b;
+
+    }
+
+    //calculadora/subtrair?a=100&b=39
+    @GetMapping("/subtrair")
+    public int subtrair(
+            @RequestParam(name = "a") int a,
+            @RequestParam(name = "b") int b) {
+        return a - b;
+    }
+}
