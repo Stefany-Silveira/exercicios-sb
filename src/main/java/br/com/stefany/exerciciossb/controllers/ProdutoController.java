377dd25218558ed7a158de7815f77697e3070004
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-26 10:52:53 -0300

    add @ Consulta de Produto Por Nome

--- src/main/java/br/com/stefany/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/stefany/exerciciossb/controllers/ProdutoController.java
@@ -32,6 +32,11 @@ public class ProdutoController {
         return produtoRepository.findAll();
     }
 
+    @GetMapping(path = "/nome/{parteNome}")
+    public Iterable<Produto> obterProdutosPorNome(@PathVariable String parteNome) {
+        return produtoRepository.findByNomeContainingIgnoreCase(parteNome);
+    }
+
     @GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
     public Iterable<Produto> obterProdutoPorPagina(@PathVariable int numeroPagina,
                                                    @PathVariable int qtdePagina) {
