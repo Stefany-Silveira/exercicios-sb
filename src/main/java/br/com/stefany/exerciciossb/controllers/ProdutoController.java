8c705555389683533afca983b62ca2326b6f1e2c
stefanysilveira <stefany.silveira21@gmail.com>
2022-08-26 09:44:49 -0300

    add @ Consultando Todos os Produtos

--- src/main/java/br/com/stefany/exerciciossb/controllers/ProdutoController.java
+++ src/main/java/br/com/stefany/exerciciossb/controllers/ProdutoController.java
@@ -21,4 +21,11 @@ public class ProdutoController {
         produtoRepository.save(produto);
         return produto;
     }
+
+    @GetMapping
+    public Iterable<Produto> obterProdutos() {
+        return produtoRepository.findAll();
+    }
+
+
 }
